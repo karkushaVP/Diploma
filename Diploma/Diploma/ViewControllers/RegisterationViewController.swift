31de02e5d97331cf382ccf950710e156ce392ab0
karkushaVP <144828076+karkushaVP@users.noreply.github.com>
2023-10-23 23:20:36 +0200

    first one

---
+++ Diploma/Diploma/ViewControllers/RegisterationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterationViewController.swift
+//  Diploma
+//
+//  Created by Polya on 23.10.23.
+//
+
+import UIKit
+
+class RegisterationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
