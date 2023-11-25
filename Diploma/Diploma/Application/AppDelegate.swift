ffa9c64cbec5e551a12ab5c80b7f08425414b0e3
karkushaVP <144828076+karkushaVP@users.noreply.github.com>
2023-11-26 00:38:08 +0100

    add final to classes

--- Diploma/Diploma/Application/AppDelegate.swift
+++ Diploma/Diploma/Application/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 import Firebase
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
